f8680abd4f1fb65ef9a2798d61da775a750b8484
Kun Yan <kyan@redhat.com>
2021-01-14 20:33:28 +0800

    fix: avoid types error when @types/express upgraded

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-const router = Router();
+export default (): Router => {
+  const router = Router();
 
-router.get('/server-health', (req, res) => {
-  res.status(200).send("I'm OK");
-});
+  router.get('/server-health', (req, res) => {
+    res.status(200).send("I'm OK");
+  });
 
-export default router;
+  return router;
+};
