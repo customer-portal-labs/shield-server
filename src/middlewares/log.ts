f344d34c700057203ff089a86ff335a88321f886
Kun Yan <kyan@redhat.com>
2021-05-20 10:24:36 +0800

    feat: add rate-limit

--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -2,22 +2,10 @@ import { Request, RequestHandler, Response, NextFunction } from 'express';
 import morgan, { StreamOptions } from 'morgan';
 import { logger } from '../logger';
 import { ShieldConfig } from '../models/Config';
+import { getIP } from '../util';
 
 export const log = (options: ShieldConfig): RequestHandler => {
-  morgan.token('remote-addr', (req: Request) => {
-    const akamaiIP = req.header('True-Client-IP');
-    const realIP = req.header('x-real-ip');
-    const forwardedIP = req.header('x-forwarded-for');
-    return (
-      akamaiIP ||
-      realIP ||
-      forwardedIP ||
-      req.ip ||
-      (req.connection && req.connection.remoteAddress) ||
-      undefined
-    );
-  });
-
+  morgan.token('remote-addr', (req: Request) => getIP(req));
   morgan.token('remote-user', (req: Request) => {
     const rhUser = req.cookies['rh_user'];
     if (rhUser) {
